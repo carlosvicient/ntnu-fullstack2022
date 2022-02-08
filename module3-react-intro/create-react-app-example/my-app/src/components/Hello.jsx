22ebe6ca63738787436fd410a7cb6622c35a35eb
Carlos Vicient <carlos.vicient@ntnu.no>
2022-02-08 14:25:17 +0100

    React intro examples lecture

---
+++ module3-react-intro/create-react-app-example/my-app/src/components/Hello.jsx
@@ -0,0 +1,14 @@
+import './Hello.css';
+
+function Hello() {
+  return (
+    <div className="Hello">
+      <p>I am an apple</p>
+      <ul>
+        <li>pen</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Hello;
